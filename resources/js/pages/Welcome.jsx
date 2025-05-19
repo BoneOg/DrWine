43da16efe0b93606b7106d86859ccdde736208bb
BoneOg <vaughnpereyra098@gmail.com>
2025-05-19 21:19:33 +0800

    Laravel + Inertia + React

---
+++ resources/js/pages/Welcome.jsx
@@ -0,0 +1,15 @@
+import { Head } from '@inertiajs/react';
+
+export default function Welcome() {
+  return (
+    <>
+      <Head title="Welcome" />
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-800">Welcome to Our App!</h1>
+        <p className="mt-4 text-gray-600 text-lg">
+          This is a basic welcome page built with Inertia.js and React.
+        </p>
+      </div>
+    </>
+  );
+}
