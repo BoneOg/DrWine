11495c276f08c39cc425a0581c002d91af039577
Joeverly Lanzaderas <lanzaderas.joeverlypearl04@gmail.com>
2025-05-23 19:33:27 +0800

    Fixed footer now responsive

--- resources/js/components/footer.jsx
+++ resources/js/components/footer.jsx
@@ -1,7 +1,16 @@
 export default function Footer() {
     return (
-        <footer className="bg-gray-900 text-white p-4 text-center">
-            <p>© 2025 My App. All rights reserved.</p>
+        <footer className="bg-gray-900 text-white p-4 text-center
+                           h-[120px] md:h-[180px] // Responsive height: 120px on small, 180px on md and up
+                           flex flex-col items-center justify-center">
+            {/* Logo on top of the text - Responsive Sizing */}
+            <img
+                src="/assets/logo.png"
+                alt="DrWine Logo"
+                className="h-12 w-12 object-cover rounded-full mb-2 md:h-20 md:w-20 md:mb-4"
+            />
+            {/* Copyright Text - Responsive Sizing */}
+            <p className="text-xs md:text-base">© DrWine. All rights reserved.</p>
         </footer>
     );
 }
